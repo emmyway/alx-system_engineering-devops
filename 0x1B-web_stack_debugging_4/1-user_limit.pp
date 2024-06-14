535879b4dd50c446bc69fa83a921435f6f0e97ae
Emmanuel Nnaji <realreaach@yahoo.com>
2024-06-14 04:35:15 +0100

    fixed for lint

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,12 +2,12 @@
 
 # thus increaee hard files limits for holberton user
 exec { 'hard-files-limit-increase-for-holberton':
-	command => "sed -i '/^holberton hard/s/4/50000/' /etc/security/limits.conf",
-	path 	=> '/usr/local/bin/:/bin/',
+  command => "sed -i '/^holberton hard/s/4/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin/',
 }
 
 # thus increase soft files limits for holberton
 exec { 'soft-files-limit-increase-for-holberton':
-	command => "sed -i '/^holberton soft/s/5/50000/' /etc/security/limits.conf",
-	path    => '/usr/local/bin/:/bin/',
+  command => "sed -i '/^holberton soft/s/5/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin/',
 }
